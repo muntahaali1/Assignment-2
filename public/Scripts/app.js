6ca285b01d76051f9891baf69f41f02d55438845
muntahaali1 <144643489+muntahaali1@users.noreply.github.com>
2023-11-02 22:33:37 -0400

    A2update

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+// IIFE --> Immediatly invoked function express 
+(function(){
+    function Start()
+    {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start);
+})();
